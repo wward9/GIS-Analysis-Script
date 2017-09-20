9d2baac66d54fccccbc824dc0b370756f35e6dec
wward9 <wward949@gmail.com>
2017-09-19 22:12:06 -0400

    Cleaned up Geocoder Script

--- GeocoderFunction.R
+++ GeocoderFunction.R
@@ -1,15 +1,19 @@
-inputfile <- "input/Walmart.csv"
-outputfile <- "output/Walmart.csv"
-
 #Install package
 #devtools::install_github("gsk3/taRifx.geo")
-setwd("C:/Users/wward/Documents/R/R Automation/MappingRCode")
-
-#Read in intput file
-inputfile <- read.csv(inputfile)
 
 #Load package
 require(taRifx.geo)
+
+#Set working directory
+setwd("C:/Users/wward/Documents/R/Example Files")
+
+#Set input and output files
+inputfile <- "Test_Store_List.csv" #Update with your input file
+outputfile <- "Test_Store_Listout.csv" #update with your output file
+
+#Read in intput file
+inputfile <- read.csv(inputfile)
+inputfile$Full.Address <- paste(inputfile$Address," ", inputfile$City, ", ", inputfile$State, " ", inputfile$ZIP) #Create Full.Address column
 
 #Specify API Key
 options(BingMapsKey='4vN5i7NOCAdYdCCvNd5J~BsrfspiO64LJg1i65T_imw~AuabdtbMdct5_2GPGZbjcdLLiAjbC1sWGMjPjtnO-1_1mls7ULPG-Js-EJ5EG_RZ')
